ea5a0612ad97b83fb3bff93b6f2240451531cc0f
cenjie <a291183649@gmail.com>
2018-09-16 16:45:08 +0800

    修复了频闪问题

--- src/main/webapp/assets/js/biz/loginCheck.js
+++ src/main/webapp/assets/js/biz/loginCheck.js
@@ -4,18 +4,20 @@ $(function () {
         url:"/user/isLogin.do",
         dataType:"json",
         success:function(res){
-            //如果已登陆，则在登陆与注册页面将跳转到主页
+            var strUrl=location.href;
+            var arrUrl=strUrl.split("/");
+            var strPage=arrUrl[arrUrl.length-1];
+            //如果已登陆，则在登陆或注册页面将跳转到主页
             if(res.status === 5){
-                var strUrl=location.href;
-                var arrUrl=strUrl.split("/");
-                var strPage=arrUrl[arrUrl.length-1];
                 if(strPage === "login.html" || strPage === "register.html") {
                     location.href="index.html"
                 }
             }
-            //如果未登陆，则跳转到登陆页面
+            //如果未登陆，且不在登陆和注册页面，则跳转到登陆页面
             if(res.status === 6){
-                location.href="login.html"
+                if(strPage !== "login.html" && strPage !== "register.html") {
+                    location.href="login.html"
+                }
             }
         }, error:function() {
             alert("向服务器请求数据失败")
