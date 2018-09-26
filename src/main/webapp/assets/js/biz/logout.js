f280bebe0f7fbae0f5b5f9d444ee55d63bd1d940
cenjie <a291183649@gmail.com>
2018-09-26 18:43:29 +0800

    18:43

--- src/main/webapp/assets/js/biz/logout.js
+++ src/main/webapp/assets/js/biz/logout.js
@@ -1,13 +1,15 @@
 function logout() {
-    $.ajax({
-        type:"GET",
-        url:"/user/logout.do",
-        dataType:"json",
-        success:function (res) {
-            location.href = "login.html";
-        },
-        error:function () {
-            alert("向服务器请求数据失败");
-        }
-    });
+    if(confirm("确认退出吗？")) {
+        $.ajax({
+            type: "GET",
+            url: "/user/logout.do",
+            dataType: "json",
+            success: function (res) {
+                window.location.replace("login.html")
+            },
+            error: function () {
+                alert("向服务器请求数据失败");
+            }
+        });
+    }
 }
