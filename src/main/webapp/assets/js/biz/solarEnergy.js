c960c66266cf944c855a7bc216483760dd4f0de6
cenjie <a291183649@gmail.com>
2018-10-02 12:27:40 +0800

    已搭建好太阳能实验图片js，暂未实现

---
+++ src/main/webapp/assets/js/biz/solarEnergy.js
@@ -0,0 +1,86 @@
+var chart1;
+var chart2;
+var chart3;
+var chart4;
+
+function generateChart1() {
+}
+
+function generateChart2() {
+}
+
+function generateChart3() {
+}
+
+function generateChart4() {
+}
+
+function uploadChart(chart, index) {
+    // 获取Canvas的编码。
+    var imgData = document.getElementById(chart).toDataURL("image/png");
+
+    // 上传到后台。
+    $.ajax({
+        type: "post",
+        url: "/exp/upload_chart.do",
+        data: {
+            image: imgData.substring(22),
+            expId: 1,
+            index: index
+        },
+        async: false,
+        success: function (res) {
+            // alert(res.status);
+        },
+        error: function (res) {
+            alert("向服务器请求数据失败" + res.msg)
+        }
+
+    })
+}
+
+
+$(function () {
+        $.ajax({
+            type: "GET",
+            url: "/exp/get_exp_status.do",
+            data: {
+                expId: 1
+            },
+            dataType: "json",
+            success: function (result) {
+                if (result.status === 10) {
+                    alert("实验已关闭，请联系实验老师");
+                    location.href = "../index.html";
+                }
+            },
+            error: function (result) {
+                alert("向服务器请求数据失败" + result);
+            }
+        });
+
+        $.ajax({
+            type: "GET",
+            url: "/score/is_stu_have_score.do",
+            data: {
+                expId: 1
+            },
+            dataType: "json",
+            success: function (res) {
+                if(res.status === 0){
+                    //用户未提交过此实验
+                } else if (res.status === 2) {
+                    location.href = "../login.html";
+                } else if(res.status === 15){
+                    alert("您已提交过此实验，如有疑问请联系实验老师");
+                    location.href = "../index.html";
+                } else{
+                    alert("服务器发生错误");
+                }
+            },
+            error: function (res) {
+                alert("向服务器请求数据失败" + res);
+            }
+        });
+    }
+)
