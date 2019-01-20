c41e5c647970b2cb150c8bbf797107dd54d44917
cenjie <a291183649@gmail.com>
2019-01-20 15:54:19 +0800

    合并完成

--- src/main/webapp/assets/js/biz/youngmodulus.js
+++ src/main/webapp/assets/js/biz/youngmodulus.js
@@ -25,6 +25,12 @@ function autoGenera1(){
     autoGenera1Helper(s60, s61, 6);
     autoGenera1Helper(s70, s71, 7);
 
+    var a1 = $("#answer2_r4_1").text();
+    var a2 = $("#answer2_r5_1").text();
+    var a3 = $("#answer2_r6_1").text();
+    var a4 = $("#answer2_r7_1").text();
+    var ans2 = (parseFloat(a1) + parseFloat(a2) + parseFloat(a3) + parseFloat(a4)) / 400;
+    $("#answer2").text(ans2);
 }
 
 function autoGenera1Helper(a, b, row){
@@ -34,9 +40,49 @@ function autoGenera1Helper(a, b, row){
     $("#answer2_r" + row).text(ans1);
     // alert(ans1);
     if(row >= 4){
-
-        var ans2 = $("#answer2_r" + row).text() - $("#answer2_r" + row - 4).text();
+        var fr = row -4;
+        var ans2 = $("#answer2_r" + row).text() - $("#answer2_r" + fr).text();
         $("#answer2_r" + row + "_1").text(ans2);
     }
 
+}
+
+function autoGenera2(){
+    var a1 = parseFloat($("#table_1_1").val());
+    var a2 = parseFloat($("#table_1_2").val());
+    var a3 = parseFloat($("#table_1_3").val());
+    var a4 = parseFloat($("#table_1_4").val());
+    var a5 = parseFloat($("#table_1_5").val());
+    var a6 = parseFloat($("#table_1_6").val());
+    var ans = (a1 + a2 + a3 + a4 + a5 + a6) / 6000;
+    var d0 = parseFloat($("#blank_2").val());
+    $("#answer1").text(ans-d0/1000);
+}
+
+function autoGenera3(){
+    var L = parseFloat($("#blank_6").val());
+    var D = parseFloat($("#blank_5").val());
+    var F = parseFloat($("#blank_3").val());
+    var d = parseFloat($("#answer1").text());
+    var b = parseFloat($("#blank_4").val());
+    var s = parseFloat($("#answer2").text());
+    console.log("L = " + L);
+    console.log("D = " + D);
+    console.log("F = " + F);
+    console.log("d = " + d);
+    console.log("b = " + b);
+    console.log("s = " + s);
+    var on = 8 * L * D * F;
+    console.log("on = " + on);
+    var down = 3.14 * d * d * b * s;
+    console.log("down = " + down)
+    var e = on / down;
+    console.log("e = " + e);
+    $("#blank_7").val(num2e(e));
+}
+
+function num2e(num){
+    var p = Math.floor(Math.log(num)/Math.LN10);
+    var n = num * Math.pow(10, -p);
+    return n.toFixed(3) + 'e' + p;
 }
